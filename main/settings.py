99394da40ecbec122262db83cecca4f993660875
Teruo <teruoutsumi@earthlink.net>
2016-04-27 23:15:00 -0700

    Comment out SECRET_KEY
    Use SECRET_KEY in secret.py instead.

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^(crg^77ww^b9l!z%kko0$w0y0e#qj0h8b#(n5iqq3)#ctf9@y'
+#SECRET_KEY = '^(crg^77ww^b9l!z%kko0$w0y0e#qj0h8b#(n5iqq3)#ctf9@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
