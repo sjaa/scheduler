80ff0d6e2ea549dee3519bad75255b1805084287
Teruo <teruoutsumi@earthlink.net>
2016-05-19 00:25:09 -0700

    Changed directory/app name

--- admin.py
+++ admin.py
@@ -1,11 +1,23 @@
 import datetime
 from django.contrib      import admin
-from .models             import EventType, Event
-from ev_sched.gen_events import calc_start_time, foo
-from ev_sched.cal_const  import DAY, TZ_LOCAL
+from .models             import AuxEvent, EventType, Event
+from sched_ev.gen_events import calc_start_time, foo
+from sched_ev.cal_const  import DAY, TZ_LOCAL, locations
 import pdb
 
 
+# For AuxEvent
+class PostAuxEvent(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date', 'notes')
+    list_filter = ('category',)
+    search_fields = ['title']
+    ordering = ('date',)
+#   actions = [event_type_copy, event_gen]
+
+
+###############
+# For EventType
+###############
 def event_gen(modeladmin, request, queryset):
     foo(queryset)
 event_gen.short_description = "Generate events from selected templates"
@@ -27,6 +39,11 @@ class PostEventType(admin.ModelAdmin):
     ordering = ('nickname',)
     actions = [event_type_copy, event_gen]
 
+
+###########
+# For Event
+###########
+'''
 def event_draft_set_planned(modeladmin, request, queryset):
     for event in queryset:
         if event.draft and not event.planned:
@@ -40,6 +57,7 @@ def event_draft_remove_planned(modeladmin, request, queryset):
             event.planned = False
             event.save()
 event_draft_remove_planned.short_description = "Make selected events unplanned"
+'''
 
 def find_draft_event_conflicts(events):
     '''
@@ -51,6 +69,7 @@ def find_draft_event_conflicts(events):
         set of draft events that conflict with other events
     '''
     # for each event, check all events that start at the same time or after
+    pdb.set_trace()
     events_copy = events[1:]
     conflict_set = set()
     # compare times for every pair of events
@@ -59,11 +78,13 @@ def find_draft_event_conflicts(events):
         for ev2 in events_copy:
             if ev1_end > ev2.date_time:
                 # ev1 ends after start of ev2
-                # if there's a conflict and the event is draft, add event for
-                # later removal
+                # if there's a conflict and the event is draft,
+                #   add event for later removal
                 if ev1.draft:
+                    print('** conflict ', ev1.title)
                     conflict_set.add(ev1)
                 if ev2.draft:
+                    print('** conflict ', ev2.title)
                     conflict_set.add(ev2)
             if ev1_end <= ev2.date_time:
                 # ev1 ends before ev2 starts
@@ -72,6 +93,14 @@ def find_draft_event_conflicts(events):
                 break
         events_copy.pop(0)
     return conflict_set
+
+
+def before_now(date_time):
+    # check if 'date_time' is before current time
+    # TODO: during test return False
+#   return date_time < TZ_LOCAL.localize(datetime.datetime.now())
+    return False
+
 
 def event_draft_accept(modeladmin, request, queryset):
     '''
@@ -85,34 +114,37 @@ def event_draft_accept(modeladmin, request, queryset):
     # Get all events between first and last draft events.
     # To account for multi-day events, set range for 7 days before
     # first draft event to 7 days after last draft event
-    if not queryset:
-        return
-    first_event = min(queryset, key=lambda x: x.date_time)
-    last_event  = max(queryset, key=lambda x: x.date_time)
-    date_start = first_event.date_time - datetime.timedelta(days=7)
-    date_end   = last_event.date_time  + datetime.timedelta(days=7)
+    date_start = queryset[0].date_time - datetime.timedelta(days=7)
+    date_end   = queryset[0].date_time + datetime.timedelta(days=7)
     events = Event.objects.filter(date_time__range=(date_start, date_end))
 
     # For each location, construct list of events
+#   events_by_loc = [None] * (len(locations
     events_by_loc = {}
     try:
-        for event in events:
-            if not event.location in events_by_loc:
+        for event in queryset:
+            if not event.date_time or before_now(event.date_time):
+                # skip events with blank date_time or in past
+                continue
+            if event.location not in events_by_loc:
                 events_by_loc[event.location] = []
             events_by_loc[event.location].append(event)
     except:
         pdb.set_trace()
     # for each location, find pairs of events whose times overlap
     conflict_set = set()
-    for l in events_by_loc.values():
-        if len(l) > 1:
+    for l_events in events_by_loc.values():
+        if len(l_events) > 1:
             # nothing to do if list doesn't have more than one event
-            l.sort(key = lambda x: x.date_time)
-            # find if two events overlap - events are sorted by start time
-            conflict_set |= find_draft_event_conflicts(l)
+            l_events.sort(key = lambda x: x.date_time)
+            # find if events overlap - events are sorted by start time
+            conflict_set |= find_draft_event_conflicts(l_events)
     for event in queryset:
-        if event in conflict_set:
-            # keep event as draft, don't touch event
+        if not event.date_time or before_now(event.date_time):
+            # blank date_time or in past: keep event as draft, don't touch event
+            continue
+        elif event in conflict_set:
+            # time conflict: keep event as draft, don't touch event
             conflict_set.remove(event)
         elif event.planned:
             # no conflict, accept event into calendar
@@ -120,20 +152,15 @@ def event_draft_accept(modeladmin, request, queryset):
             event.save()
 event_draft_accept.short_description = "Accept selected draft events"
 
-def event_draft_set(modeladmin, request, queryset):
+def event_copy(modeladmin, request, queryset):
     for event in queryset:
-        event.draft = True
-        event.save()
-event_draft_set.short_description = "Undo accept for selected events"
-
-def event_draft_copy(modeladmin, request, queryset):
-    for event in queryset:
-        if event.draft and event.planned:
+        if event.planned:
             new_event = event
             new_event.pk = None
+            new_event.draft = True
             new_event.nickname += ' (Copy)'
             event.save()
-event_draft_copy.short_description = "Copy selected draft events"
+event_copy.short_description = "Copy selected draft events"
 
 def event_draft_delete(modeladmin, request, queryset):
     for event in queryset:
@@ -161,20 +188,21 @@ def event_mv_week_after(modeladmin, request, queryset):
         event.save()
 event_mv_week_after.short_description = "Move selected draft event dates one week after"
 
-class PostEvent(admin.ModelAdmin):
+class PostDraftEvent(admin.ModelAdmin):
     list_display  = ('nickname', 'draft', 'planned', 'date_chg', 'verified',
                      'category', 'date_time', 'time_length', 'location',
                      'notes')
-    list_filter   = ('planned', 'draft', 'event_type', 'category', 'location')
+#   list_editable = ('planned',)
+    list_filter   = ('draft', 'planned', 'event_type', 'category', 'location')
     search_fields = ['nickname']
     ordering      = ('date_time',)
-    actions       = [event_draft_copy,
+    actions       = [event_copy,
                      event_mv_week_before   , event_mv_week_after,
                      event_draft_set_planned, event_draft_remove_planned,
-                     event_draft_accept     , event_draft_set,
-                     event_draft_delete]
+                     event_draft_accept     , event_draft_delete]
     list_per_page = 250
 
 
+admin.site.register(AuxEvent, PostAuxEvent)
 admin.site.register(EventType, PostEventType)
-admin.site.register(Event, PostEvent)
+admin.site.register(Event, PostDraftEvent)
