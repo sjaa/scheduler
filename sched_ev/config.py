fb706e4c2d5fae85110dcf3466a49a8daa0f3059
Teruo <teruoutsumi@earthlink.net>
2016-05-30 17:29:39 -0700

    Added '.value' to event_category definition, "Other" to locations_ps

--- sched_ev/config.py
+++ sched_ev/config.py
@@ -36,24 +36,25 @@ class EventCategory(Enum):
 #   observers    = 'ob'
 #   imagers      = 'im'
 
-event_category = {EventCategory.public      : 'public'      ,
-                  EventCategory.member      : 'member'      ,
-                  EventCategory.volunteer   : 'volunteer'   ,
-                  EventCategory.coordinator : 'coordinator' ,
-                  EventCategory.private     : 'private'     ,
-                  EventCategory.board       : 'board'       ,
-                  EventCategory.external    : 'external'     }
+event_category = {EventCategory.public     .value : 'public'      ,
+                  EventCategory.member     .value : 'member'      ,
+                  EventCategory.volunteer  .value : 'volunteer'   ,
+                  EventCategory.coordinator.value : 'coordinator' ,
+                  EventCategory.private    .value : 'private'     ,
+                  EventCategory.board      .value : 'board'       ,
+                  EventCategory.external   .value : 'external'     }
 
 
 locations_gps = {
     #     location / latitude / longitude (- is west) / elevation (meters)
-    1 : ("Houge Park, Blg. 1"                , '37.257482', '-121.941998',   50),  # indoor
-    2 : ("Houge Park"                        , '37.257471', '-121.942331',   50),  # outdoor
-    3 : ("Rancho Cañada del Oro"             , '37.147076', '-121.775296',  200),
-    4 : ("Mendoza Ranch"                     , '37.070585', '-121.522456',  300),
-    5 : ("Coyote Valley"                     , '37.172142', '-121.729031',  100),
-    6 : ("Pinnacles Nat'l Park, East Side"   , '36.489659', '-121.150614',  500),
-    7 : ("Pinnacles Nat'l Park, West Side"   , '36.491788', '-121.209606',  500),
-    8 : ("Yosemite Nat'l Park, Glacier Point", '37.727884', '-119.572965', 2800),
+     1 : ("Houge Park, Blg. 1"                , '37.257482', '-121.941998',   50),  # indoor
+     2 : ("Houge Park"                        , '37.257471', '-121.942331',   50),  # outdoor
+     3 : ("Rancho Cañada del Oro"             , '37.147076', '-121.775296',  200),
+     4 : ("Mendoza Ranch"                     , '37.070585', '-121.522456',  300),
+     5 : ("Coyote Valley"                     , '37.172142', '-121.729031',  100),
+     6 : ("Pinnacles Nat'l Park, East Side"   , '36.489659', '-121.150614',  500),
+     7 : ("Pinnacles Nat'l Park, West Side"   , '36.491788', '-121.209606',  500),
+     8 : ("Yosemite Nat'l Park, Glacier Point", '37.727884', '-119.572965', 2800),
+    99 : ("Other"                             , '0'        , '0'          ,    0),
 }
 
