45707716eb06457608d04c7cc6bc6e54c3bb2061
Teruo <teruoutsumi@earthlink.net>
2016-05-30 17:25:21 -0700

    Commented out debug toolbar

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+#   'debug_toolbar',
     'sched_ev'
 )
 
