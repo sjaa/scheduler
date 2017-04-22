aaa31e8812ffdc54832b0cea7cc3377194137dbe
Teruo <teruoutsumi@earthlink.net>
2017-04-22 11:46:40 -0700

    Added details url

--- sched_announce/urls.py
+++ sched_announce/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
-from . import views
+from .                import views
 
 urlpatterns = [
     # event draft views
     url(r'^search$', views.search,
-                     name='search'),
-#   url(r'^aux_events/(?P<year>\d{4})/(?P<order>[-\w]*)$',
+                     name='announce-search'),
     url(r'^(?P<period>\d{6}-\d{6})/ch=(?P<channel>\d*)/loc=(?P<location>\d*)/event_type=(?P<event_type>\d*)',
                      views.announce_details,
                      name='announce_details')
