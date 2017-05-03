899d4843e2d5edd2416ac5fe349f2ec789ead2ba
Teruo <teruoutsumi@earthlink.net>
2017-05-03 01:26:19 -0700

    Add renew action

--- membership/admin.py
+++ membership/admin.py
@@ -4,15 +4,23 @@ from   django.contrib        import admin
 from   sched_core.sched_log  import sched_log
 from   sched_core.filters    import AdminDateTimeYearFilter
 from   .models               import User
+from   .views                import renew
 
 
-###############
-# For EventType
-###############
+################
+# For Membership
+################
+
+
+# action from admin event page
+def renew_memberships(modeladmin, request, queryset):
+#   renew(modeladmin, request, queryset)
+    views.renew(queryset)
+renew_memberships.short_description = "Renew memberships"
 
 
 class PostUsers(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'status',
+    list_display = ('username', 'first_name', 'last_name', 'modified', 'status',
                     'date_start', 'date_end', 'date_since', 'email', 'notes')
 #   list_filter = ('status', 'date_end', 'groups', 'coordinator')
     list_filter = ('status', 'date_end', 'volunteer')
@@ -20,12 +28,13 @@ class PostUsers(admin.ModelAdmin):
 #   ordering = ('status', 'last_name', 'first_name')
 #   ordering = ('status', last_name, first_name)
     ordering = ('status',)
+
     fields = ('status', 'username', 'first_name', 'last_name', 'email',
               ('date_start', 'date_end', 'date_since'),
               'addr1', 'addr2', ('city', 'state', 'zip_code'),
               'phone1', 'phone2',
               'notices', 'associate', 'notes', 'volunteer', 'groups', 'coordinator')
-#   actions = [event_type_copy, event_gen]
+    actions = [renew]
 
 # TODO: add to list_filter later?
 #   volunteer   = models.BooleanField(default=False, choices=L_BOOLEAN)
