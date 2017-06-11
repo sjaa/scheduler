b532c2284efac4d9a77293208d80bd92e3da17dd
Teruo <teruoutsumi@earthlink.net>
2017-06-11 13:59:21 -0700

    Add file

--- sched_core/get_events.py
+++ sched_core/get_events.py
@@ -1,3 +1,4 @@
+import pdb
 import datetime
 
 from django.db.models  import Q
@@ -5,17 +6,39 @@ from django.db.models  import Q
 from sched_ev.models   import Event, L_MONTH
 from .config           import TZ_LOCAL, site_names, end_of_month
 
-
-def get_events(period, location, event_type):
+def parse_period(period, month=False, date=False):
     # get starting date
     start_year  = int(period[ 0: 4])
     start_month = int(period[ 4: 6])
-    start = TZ_LOCAL.localize(datetime.datetime(start_year, start_month, 1))
+    if date:
+        start_date = int(period[ 6: 8])
+        period = period[9:]
+    elif month:
+        start_date = 1
+        period = period[7:]
+    start = TZ_LOCAL.localize(datetime.datetime(start_year, start_month, start_date))
+
     # get ending date, just before midnight
-    end_year    = int(period[ 7:11])
-    end_month   = int(period[11:13])
-    # get last day in month - monthrange returns (first day, last day)
-    end   = TZ_LOCAL.localize(end_of_month(end_year, end_month))
+    end_year  = int(period[ 0: 4])
+    end_month = int(period[ 4: 6])
+    if date:
+        end_date = int(period[ 6: 8])
+    elif month:
+        end_date = end_of_month(end_year, end_month)
+    end = TZ_LOCAL.localize(datetime.datetime(end_year, end_month, end_date))
+    if date:
+        period_str = '{} {}, {}&nbsp -&nbsp {} {}, {}'.format(
+                        L_MONTH[start_month-1][1], start_date, start_year,
+                        L_MONTH[end_month  -1][1], end_date  , end_year)
+    elif month:
+        period_str = '{} {}&nbsp -&nbsp {} {}'.format(
+                        L_MONTH[start_month-1][1], start_year,
+                        L_MONTH[end_month  -1][1], end_year)
+    return (start, end, period_str)
+
+
+def get_events(period, location, event_type):
+    start, end, period_str = parse_period(period, month=True)
     # detect bad period if start is after end
     bad_period = True if start > end else False
     # get events
@@ -27,10 +50,7 @@ def get_events(period, location, event_type):
         event_type = int(event_type)
         q &= Q(event_type=int(event_type))
     events = Event.objects.filter(q).order_by('date_time')
-    if start < end:
-        period = '{} {} - {} {}'.format(L_MONTH[start_month-1][1], start_year,
-                                        L_MONTH[end_month  -1][1], end_year)
-    else:
+    if start >= end:
         period = 'bad period'
-    return (events, period)
+    return (events, period_str)
 
